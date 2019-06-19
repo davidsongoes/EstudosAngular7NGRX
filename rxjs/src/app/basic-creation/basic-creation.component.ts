3358b8cef8422efa1b3f1d98e020f31ba2161322
davidsongoes <davidsongoes@gmail.com>
2019-06-19 10:53:43 -0300

    timer e comentários sobre os operadores

--- rxjs/src/app/basic-creation/basic-creation.component.ts
+++ rxjs/src/app/basic-creation/basic-creation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Observer, from, of, interval } from 'rxjs';
+import { Observable, Observer, from, of, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-basic-creation',
@@ -24,19 +24,24 @@ export class BasicCreationComponent implements OnInit {
   }
 
   public fromClick(): void{
-    from([1,2,3,4,5,{x:10, y: 20}]).subscribe((v) => console.log(v));
+    from([1,2,3,4,5,{x:10, y: 20}]).subscribe((v) => console.log(v)); //retorna os dados passados no parâmetro separados, objeto não é separado.
     const source = from([1,2,3,4,5,{x:10, y: 20}]);
     source.subscribe((v) => console.error(v));
     source.subscribe((v) => console.warn(v));
   }
 
   public ofClick(): void {
-    of([1,2,3,4,5,{x:10, y: 20}]).subscribe((v) => console.log(v));
+    of([1,2,3,4,5,{x:10, y: 20}]).subscribe((v) => console.log(v)); //retorna dados conforme passado dentro do operador.
   }
 
   public intervalClick(): void {
-    const source = interval(1000);
+    const source = interval(1000); //executa em intervalos conforme tempo passado no parâmetro.
     source.subscribe((v) => console.log(v));
   }
 
+  public timerClick(): void {
+    // const source = timer(2000); //executa somente uma vez depois do delay.
+    const source = timer(3000, 1000); //executa depois do delay e fica executando conforme tempo passado no segundo parâmetro.
+    source.subscribe((v) => console.log(v));
+  }
 }
