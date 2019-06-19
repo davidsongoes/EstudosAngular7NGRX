0941055c776b8553c92f5f79088cc7574a9ea7f1
davidsongoes <davidsongoes@gmail.com>
2019-06-19 10:14:34 -0300

    Criando projeto da aula de operadores do RxJS

---
+++ rxjs/src/app/basic-creation/basic-creation.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, from } from 'rxjs';
+
+@Component({
+  selector: 'app-basic-creation',
+  templateUrl: './basic-creation.component.html',
+  styleUrls: ['./basic-creation.component.css']
+})
+export class BasicCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public observableCreate(): void {
+    const hello = Observable.create((observer: Observer<string>) => {
+      observer.next("Hello");
+      observer.next("from");
+      observer.next("Observable");
+      observer.complete();
+    });
+    hello.subscribe((val) => console.log(val));
+  }
+
+  public fromClick(): void{
+    from([1,2,3,4,5,{x:10, y: 20}]).subscribe((v) => console.log(v));
+    const source = from([1,2,3,4,5,{x:10, y: 20}]);
+    source.subscribe((v) => console.error(v));
+    source.subscribe((v) => console.warn(v));
+  }
+
+}
