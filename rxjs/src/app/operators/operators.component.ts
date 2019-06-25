7848e402cd444f71a799143bdbe4691d0aaa6aa9
Davidson Góes <davidsongoes@gmail.com>
2019-06-25 12:51:30 -0300

    filter

--- rxjs/src/app/operators/operators.component.ts
+++ rxjs/src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from, fromEvent } from 'rxjs';
-import { map, delay } from 'rxjs/operators';
+import { from, fromEvent, interval } from 'rxjs';
+import { map, delay, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-operators',
@@ -15,18 +15,34 @@ export class OperatorsComponent implements OnInit {
   }
 
   public mapClick(): void {
-    from([1,2,3,4,5,6,7,8,9,0])
-    .pipe(
-      map((i) => i * 2),
-      map((i) => i / 2),
-      delay(3000)
-    )
-    .subscribe((v) => console.log(v));
+    from([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+      .pipe(
+        map((i) => i * 2),
+        map((i) => i / 2),
+        delay(3000)
+      )
+      .subscribe((v) => console.log(v));
 
     fromEvent(document, 'click')
-    .pipe(
-      map((event: MouseEvent) => ({X: event.screenX, Y: event.screenY}))
-    )
-    .subscribe((pos) => console.log(pos))
+      .pipe(
+        map((event: MouseEvent) => ({ X: event.screenX, Y: event.screenY }))
+      )
+      .subscribe((pos) => console.log(pos))
+  }
+
+  public filterClick(): void {
+    from([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+      .pipe(
+        filter((i) => i % 2 == 1)
+      )
+      .subscribe((v) => console.log(v));
+
+    interval(1000)
+      .pipe(
+        filter((i) => i % 2 == 0),
+        map((i) => "Value: " + i),
+        delay(1000)
+      )
+      .subscribe((v) => console.log(v));
   }
 }
