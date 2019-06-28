b87c4e3a36166d3ed4d52b68dbdccb6d4bb90102
Davidson Góes <davidsongoes@gmail.com>
2019-06-28 11:02:56 -0300

    async

--- rxjs/src/app/async/async.component.ts
+++ rxjs/src/app/async/async.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, toArray, delay } from 'rxjs/operators';
 
+interface User {
+  login: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-async',
   templateUrl: './async.component.html',
@@ -10,6 +15,8 @@ import { map, toArray, delay } from 'rxjs/operators';
 export class AsyncComponent implements OnInit {
 
   options$: Observable<string[]>;
+
+  logins$: Observable<User>;
 
   constructor() { }
 
@@ -22,13 +29,32 @@ export class AsyncComponent implements OnInit {
         observer.complete();
       }
     )
-    .pipe(
-      map(s => s+'!'),
-      toArray(),
-      delay(2000)
-    );
+      .pipe(
+        map(s => s + '!'),
+        toArray(),
+        delay(1000)
+      );
 
-    this.options$.subscribe(s => console.log(s));
+    // this.options$.subscribe(s => console.log(s));
+
+    this.logins$ = new Observable<User>((observer) => {
+      let names = ['Mr. Davidson', 'Mr. Raphael', 'Mr. Davi', 'Mr. Mister'];
+      let logins = ['davidson', 'raphael', 'davi', 'mister'];
+      let i = 0;
+      console.log('Here in logins$');
+      setInterval(() => {
+        if (i == 4)
+          observer.complete();
+        else
+          observer.next({login: logins[i], name: names[i]});
+          i++;
+      }, 3000);
+    });
+    this.logins$.subscribe(
+      (s) => console.log(s),
+      (error) => console.log(error),
+      () => console.log("Completed!")
+      
+      );
   }
-
 }
