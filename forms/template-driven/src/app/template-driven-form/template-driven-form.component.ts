ef4fe7c388460c4fe34e5bfa690d2307ef57a20f
Davidson Góes <davidsongoes@gmail.com>
2021-10-04 16:34:32 -0300

    feat(form): :sparkles: Adiciona form simple

---
+++ forms/template-driven/src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from './client.model';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.css'],
+})
+export class TemplateDrivenFormComponent implements OnInit {
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    birth: new Date(),
+    gender: '',
+    state: '',
+    street: '',
+    city: '',
+    phone1: '',
+    phone2: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('submit');
+  }
+}
