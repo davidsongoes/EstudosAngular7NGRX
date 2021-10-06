2d47eabe42379e785796dd8dd03e89e97fee5e58
Davidson Góes <davidsongoes@gmail.com>
2021-10-06 14:25:47 -0300

    feat(form-array): :keyboard: Aula Form Array 2-3

--- forms/template-driven/src/app/form-array/form-array.component.ts
+++ forms/template-driven/src/app/form-array/form-array.component.ts
@@ -1,36 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-array',
   templateUrl: './form-array.component.html',
-  styleUrls: ['./form-array.component.css']
+  styleUrls: ['./form-array.component.css'],
 })
 export class FormArrayComponent implements OnInit {
-  
-  clientForm!: FormGroup;
+  clientForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    adress: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+    }),
+    phones: this.fb.array([]),
+    childrens: this.fb.array([]),
+  });
+  phones = this.clientForm.get('phones') as FormArray;
+  childrens = this.clientForm.get('childrens') as FormArray;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.clientForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      adress: this.fb.group({
-        street: [''],
-        city: [''],
-        state: ['']
-      }),
-      phone: [''],
-      children: this.fb.group({
-        name: [''],
-        age: ['']
-      })
-    })
+    // this.addPhone();
+    // this.addChild();
   }
 
   onSubmit(): void {
-    console.log(this.clientForm.value)
+    console.log(this.clientForm.value);
   }
 
+  addPhone(): void {
+    this.phones.push(this.fb.control(''));
+  }
+
+  addChild(): void {
+    this.childrens.push(
+      this.fb.group({
+        name: [''],
+        age: [''],
+      })
+    );
+  }
 }
