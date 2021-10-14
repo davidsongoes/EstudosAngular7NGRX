5ee66e2e3e42e583ba418d02876438cfe83ff78c
Davidson Góes <davidsongoes@gmail.com>
2021-10-14 10:12:53 -0300

    feat(API): :sparkles: Adiciona rotas para departments

--- application-crud/server/app.js
+++ application-crud/server/app.js
@@ -1,19 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const department_controller = require("./department_controller");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/http_app", {
-  useNewUrlParser: true,
-});
+mongoose
+  .connect("mongodb://localhost:27017/http-client", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("MongoDB Conectado");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.use("/departments", departament_controller);
-app.use("/products", product_controller);
+app.use("/departments", department_controller);
+// app.use("/products", product_controller);
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server ativo na porta 3000"));
