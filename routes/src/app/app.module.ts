bc8e209d7b97e175857f75e76b63944d948484ea
Davidson Góes <davidsongoes@gmail.com>
2021-10-28 14:11:54 -0300

    feat(components): :sparkles: Implementa componentes iniciais

--- routes/src/app/app.module.ts
+++ routes/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookComponent } from './book/book.component';
+import { DvdComponent } from './dvd/dvd.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookComponent, DvdComponent, PageNotFoundComponent],
   imports: [BrowserModule, MaterialModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
