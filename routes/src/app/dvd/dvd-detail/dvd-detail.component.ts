26361f74b6cf9f57a0dc15610f93df875694c20f
Davidson Góes <davidsongoes@gmail.com>
2021-10-29 11:55:36 -0300

    feat(detail): :sparkles: Implementa paramêtro da rota

--- routes/src/app/dvd/dvd-detail/dvd-detail.component.ts
+++ routes/src/app/dvd/dvd-detail/dvd-detail.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DvdService } from 'src/app/core/dvd.service';
+import { Dvd } from 'src/app/models/dvd';
 
 @Component({
   selector: 'app-dvd-detail',
   templateUrl: './dvd-detail.component.html',
-  styleUrls: ['./dvd-detail.component.css']
+  styleUrls: ['./dvd-detail.component.css'],
 })
 export class DvdDetailComponent implements OnInit {
+  index: number | undefined;
+  dvds$: Observable<Dvd[]> | undefined;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private dvdService: DvdService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.index = +this.activatedRoute.snapshot.paramMap.get('index')!;
+    console.log(this.index);
 
+    // Tipos de get dos parametros da rota
+    console.log(
+      'Index 1: ',
+      this.activatedRoute.snapshot.paramMap.get('index')
+    );
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) =>
+      console.log('Index 2: ', params.get('index'))
+    );
+    console.log('Index 4: ', this.activatedRoute.snapshot.params['index']);
+    this.activatedRoute.params.subscribe((params) =>
+      console.log('Index 5: ', params.index)
+    );
+  }
 }
